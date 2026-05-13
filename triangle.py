182c7f433850e1c720cf4ed2ce0e4618800c5077
usmansalifu34-pixel <usmansalifu34@gmail.com>
2026-05-13 13:46:07 +0100

    this program uses only one formula to calculate the area of a triangle and it takes only 2 inputs. it will be improved later on

---
+++ triangle.py
@@ -0,0 +1,5 @@
+print("welcome to my program")
+height =  float(input("enter the height of the triangle:"))
+base = float(input("enter the base of the triangle:"))
+area = 0.5 * base * height
+print("the area of the triangle is:", round(area, 2))
